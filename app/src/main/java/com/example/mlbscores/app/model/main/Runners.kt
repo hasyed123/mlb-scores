9de8f528bd0f2537430563d83cc32e76aa5407e1
Hasan Syed <hsyed@algomau.ca>
2022-10-11 22:31:57 -0400

    live scores

---
+++ app/src/main/java/com/example/mlbscores/app/model/main/Runners.kt
@@ -0,0 +1,3 @@
+package com.example.mlbscores.app.model.main
+
+data class Runners(val first: Boolean, val second: Boolean, val third: Boolean)
--- app/src/main/java/com/example/mlbscores/app/model/main/Score.kt
+++ app/src/main/java/com/example/mlbscores/app/model/main/Score.kt
@@ -1,3 +1,10 @@
 package com.example.mlbscores.app.model.main
 
-data class Score(val team1: String, val team2: String)
+data class Score(
+    val team1: String,
+    val team2: String,
+    val runners: Runners?,
+    val inning: Int,
+    val completed: Boolean,
+    val topBottom: InningStatus
+    )
